927a664480db9e56979adb77679a647bc5708f84
Scott Walton <scott.walton@mypebble.co.uk>
2016-07-22 10:53:02 +0100

    Bump setup with RST readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 try:
-    with open('README.txt') as f:
+    with open('README.rst') as f:
         LONG_DESCRIPTION = f.read()
 except:
     LONG_DESCRIPTION = ''
@@ -12,7 +12,7 @@ setup(
     author_email='scott.walton@mypebble.co.uk',
     description='A simple, customisable, feature flipper',
     url='https://github.com/mypebble/django-feature-flipper.git',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     license='MIT',
     long_description=LONG_DESCRIPTION,
