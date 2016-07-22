bab7c1c139067c7d25e5e81f482ec419e8c81e20
Scott Walton <scott.walton@mypebble.co.uk>
2016-07-22 11:48:25 +0100

    Setup the admin to figure out what the filters all need to be

--- feature_flipper/admin.py
+++ feature_flipper/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import ManyToManyField
 
 from .models import get_feature_model
 
@@ -6,14 +7,36 @@ from .models import get_feature_model
 FeatureFlipper = get_feature_model()
 
 
-@admin.register(FeatureFlipper)
+def _is_m2m(user_field):
+    field = FeatureFlipper._meta.get_field_by_name(user_field)[0]
+    return isinstance(field, ManyToManyField)
+
+
+def get_horizontal_filter():
+    """If the USER_FEATURE_FIELD is a M2M field, set it to filter_horizontal.
+    """
+    user_field = FeatureFlipper.USER_FEATURE_FIELD
+    return user_field if _is_m2m(user_field) else None
+
+
+def get_flat_filter():
+    """If USER_FEATURE_FIELD is _not_ M2M, just return the regular fields.
+    """
+    user_field = FeatureFlipper.USER_FEATURE_FIELD
+
+    return ('feature', user_field) if not _is_m2m(user_field) else ('feature',)
+
+
 class FeatureFlipperAdmin(admin.ModelAdmin):
     """Manage the Feature Flipper
     """
-    filter_horizontal = FeatureFlipper.USER_FEATURE_FIELD,
+    filter_horizontal = get_horizontal_filter()
 
     list_display = 'feature', 'everyone'
-    list_filter = 'feature',
+    list_filter = get_flat_filter()
     list_editable = 'everyone',
 
     search_fields = 'feature',
+
+
+admin.site.register(FeatureFlipper, FeatureFlipperAdmin)
